941408b8000f18b49329036be34f6eef683d49bd
Tomes <mauricio.tome@gmail.com>
2021-08-13 19:02:39 -0300

    Updated Q3 with length and case checking

--- Q3.c
+++ Q3.c
@@ -1,9 +1,39 @@
 #include <stdio.h>
 #include <string.h>
 
+int ValidateStr(char* str){
+    int valid = 1;
+    // validate length
+    if ((strlen(str) <= 100) && (strlen(str) > 0)) {
+
+        // validate characters (lowercase only)
+        for (int i=0; i< strlen(str); i++){
+            if ((str[i] >= 'a') && (str[i] <= 'z')) {
+            } else {
+                printf("All chars must be lowercase letters\n");
+                valid = 0;
+                break;
+            }
+        }
+
+    } else {
+        printf("String must be between 1 and 100 chars long.\n");
+        valid = 0;
+    }
+
+    return valid;
+}
+
+int ValidateK(int k){
+    int valid = 1;
+    if ((k <= -1) || (k > 200)){
+        printf("Invalid  number. Valid range: 0-200\n");
+        valid = 0;
+    }
+}
+
 char * ConcatRemove(char *s, char *t, int k) {
-    
-    int i, j;
+    int i;
     int len_s = 0; int len_t = 0;
     char *ans;
 
@@ -23,8 +53,7 @@ char * ConcatRemove(char *s, char *t, int k) {
     int ins = len_t - i;    // chars to insert into s to match t
     int total = del + ins;
 
-
-    printf("String matches until char %i\n", i);
+    // printf("String matches until char %i\n", i);
     printf("delete: %i, insert: %i, total: %i\n", del, ins, total);
     if (total == k) {
         ans = "yes";
@@ -34,18 +63,30 @@ char * ConcatRemove(char *s, char *t, int k) {
     return ans;
 }
 
-void main() {
-    char s[100]; // = "teste12345";
-    char t[100]; // = "teste"
-    int k = 7;   // = 5
-    printf("Enter initial string: ");
-    scanf("%s", s);
+void main(){
+    int valid = 0;
+    int k = 0;
+    char s[100], t[100];
+    while (!valid) {
+        printf("Enter source string (1-100 chars, a-z, no numbers, spaces or punctuation:\n");
+        scanf("%s", s);
+        valid = ValidateStr(s);
+    }
     
-    printf("Enter desired (output) string: ");
-    scanf("%s", t);
-    
-    printf("Target number of operations: ");
-    scanf("%i", &k);
+    valid = 0;
+    while (!valid) {
+        printf("Enter target string (1-100 chars, a-z, no numbers, spaces or punctuation:\n");
+        scanf("%s", t);
+        valid = ValidateStr(t);
+    }
+
+    valid = 0;
+    while(!valid) {
+        printf("Enter number of operations (removals, appends) [1-200]:\n");
+        scanf("%d", &k);
+        valid = ValidateK(k);
+
+    }
 
     printf("Can we transform '%s' into '%s' with %i operations?: %s", s, t, k, ConcatRemove(s, t, k));
 }
