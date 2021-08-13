dc273da7bf229a75e976e74d65094655cbaa60e6
Tomes <mauricio.tome@gmail.com>
2021-08-13 16:52:21 -0300

    Final commit Q4

--- Q4_query.sql
+++ Q4_query.sql
@@ -1,14 +1,16 @@
--- This one is a bit hacky, we're using "Value" instead of "Grade"
-SELECT 
-	CASE
-    	when
-            value < 70 THEN 'null'
-        	ELSE name
-	END as name,
-    grade, value
-from
-	Students, Notes
-where
-	grade = (SELECT grade from Notes where (Students.value BETWEEN notes.min_value and notes.max_value))
-order by grade DESC, name ASC, value ASC
+SELECT
+    CASE
+    	WHEN (SELECT Grade FROM Notes WHERE (Students.value BETWEEN notes.min_value AND notes.max_value))
+			< 8 THEN "NULL"
+        ELSE Name
+    	END
+    AS Name,
+	(SELECT Grade FROM Notes WHERE (Students.value BETWEEN notes.min_value AND notes.max_value))
+    as Grade,
+	Value
+FROM Students
+ORDER BY
+	Grade DESC,
+    Name ASC,
+    Value ASC
 ;
